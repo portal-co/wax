633f4fcd7afa66233c7e7042fe2eb8174771bbe2
Graham Kelly <gkgoat6700@gmail.com>
2025-10-27 23:52:27 -0400

    staticize

--- crates/wax-core/src/lib.rs
+++ crates/wax-core/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
-use core::ops::Range;
+use core::{mem::transmute, ops::Range};
 
-use alloc::{boxed::Box, vec::Vec};
+use alloc::{borrow::Cow, boxed::Box, vec::Vec};
 use wasm_encoder::{BlockType, Function, Instruction, reencode::Reencode};
 use wasmparser::Operator;
 
@@ -11,3 +11,13 @@ extern crate alloc;
 pub mod build;
 pub mod lowering;
 pub mod rewrite;
+pub fn r#static(a: Instruction<'_>) -> Instruction<'static> {
+    match a {
+        Instruction::BrTable(a, b) => Instruction::BrTable(Cow::Owned(a.into_owned()), b),
+        Instruction::TryTable(a, b) => Instruction::TryTable(a, Cow::Owned(b.into_owned())),
+        Instruction::TypedSelectMulti(a) => {
+            Instruction::TypedSelectMulti(Cow::Owned(a.into_owned()))
+        }
+        a => unsafe { transmute(a) },
+    }
+}
